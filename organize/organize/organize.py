7e816d3b10b0f8dfd2e80bf0d00cffa5a21b594f
leokhachatorians <leokhachatorians@gmail.com>
2016-03-28 23:48:12 -0700

    Moving Files
    Added the basics of the files being moved around and stuff

--- organize/organize/organize.py
+++ organize/organize/organize.py
@@ -1,11 +1,41 @@
 import sys
+import shutil
+import os
+import json
+
 from config import MainDisplay
 from the_parser import parser
 
+def do_stuff():
+    config = get_config()
+    for file in os.listdir():
+        if os.path.isfile(file):
+            create_and_move(config, file)
+
+def get_config():
+    with open(os.path.dirname(os.path.realpath(__file__))+'/config.json') as config:
+        valid_config = json.load(config)
+        return valid_config
+
+def print_matching_extensions(config, extension):
+    for folder in config:
+        if extension in config[folder]:
+            print('Found {0} in {1}'.format(extension, folder))
+
+def create_and_move(config, file):
+    extension = os.path.splitext(file)[1]
+    for folder in config:
+        if extension in config[folder]:
+            if not os.path.exists('/'+folder):
+                os.makedirs(folder, exist_ok=True)
+            shutil.move(file, './'+folder)
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    config = MainDisplay()
 
     if args.choice == 'config':
+        config = MainDisplay()
         config.run()
 
+    do_stuff()
